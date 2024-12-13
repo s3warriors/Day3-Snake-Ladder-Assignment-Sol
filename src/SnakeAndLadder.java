2048d4c921086a34aa659ea4e99ee720d52a5f99
s3warriors <satveerswami982@gmail.com>
2024-12-13 12:33:30 +0530

    Added: Check for Options (No Play, Ladder, Snake)

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,9 +1,26 @@
 import java.util.Random;
 
-public class SnakeAndLadder {
+public class SnakeAndLadder{
     public static void main(String[] args) {
         Random random = new Random();
-        int dieRoll = random.nextInt(6) + 1; // Generates a number between 1 and 6
-        System.out.println("Die rolled: " + dieRoll);
+        int dieRoll = random.nextInt(6) + 1;
+        int option = random.nextInt(3); // 0: No Play, 1: Ladder, 2: Snake
+        int position = 0;
+
+        switch (option) {
+            case 0: // No Play
+                System.out.println("No Play. Current position: " + position);
+                break;
+            case 1: // Ladder
+                position += dieRoll;
+                System.out.println("Ladder! Move ahead to position: " + position);
+                break;
+            case 2: // Snake
+                position -= dieRoll;
+                position = Math.max(position, 0); // Ensure position doesn't go below 0
+                System.out.println("Snake! Move back to position: " + position);
+                break;
+        }
     }
 }
+
